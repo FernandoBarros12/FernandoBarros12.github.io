4246e3c930f9c5329020f7da15977a49aee26a18
Fernando Barros <jbarros.1202@gmail.com>
2025-07-01 22:48:28 -0500

    fix: delete identifiers and add alias config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
+
+import { fileURLToPath } from 'url'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: "https://fernandobarros12.github.io",
+  vite: {
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
+    }
+  }
 })
+
